b9c08225af5a4674de9f5a93757dc1e1f7654320
Joshua Dev <tu@email.com>
2025-12-08 23:44:02 -0500

    Fix: Remove duplicate base property in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SGMI-Frontend/',
   plugins: [react()],
   base: '/SGMI-Frontend/',
   server: {
